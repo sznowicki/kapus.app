b9691a26f8517e66fb685a12fb7d90d992801425
Szymon Nowicki <sznowicki@eskalacja.com>
2024-04-01 11:33:59 +0200

    fix: ensure assets are invalidated between deployments

--- app/lib/ssr-render.js
+++ app/lib/ssr-render.js
@@ -1,11 +1,20 @@
 import Mustache from 'mustache';
 import { readFileSync } from "fs";
 import { resolve } from 'path';
+import { randomBytes } from 'crypto';
 
 const beforeTemplate = readFileSync(resolve(import.meta.dirname, './partials/before.html'), 'utf-8');
 const afterTemplate = readFileSync(resolve(import.meta.dirname, './partials/after.html'), 'utf-8');
 
-export const renderHtml = async (template, data) => {
+// Random short hash via crypto
+const hash = randomBytes(6).toString('hex');
+
+export const renderHtml = async (template, incomingData) => {
+	const data = {
+		hash,
+		...incomingData,
+	}
+
 	const before = Mustache.render(beforeTemplate, data);
 	const after = Mustache.render(afterTemplate, data);
 
