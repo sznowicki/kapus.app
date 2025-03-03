80e5fd0999334a8b9f97f67003ea09222d573c7e
Szymon Nowicki <hey@nowicki.io>
2025-03-03 18:28:37 +0100

    feat: new rooms pattern

--- app/controllers/room/renderRoom.js
+++ app/controllers/room/renderRoom.js
@@ -1,5 +1,8 @@
 import {SessionRoom} from '../../lib/SessionRoom.js';
 import {renderHtml} from '../../lib/ssr-render.js';
+import {readFileSync} from 'fs';
+import {resolve} from 'path';
+
 const roomTemplate = readFileSync(resolve(import.meta.dirname, './room.template.html'), 'utf-8');
 const expiredRoomTemplate = readFileSync(resolve(import.meta.dirname, './expired-room.template.html'), 'utf-8');
 
